1567abbf5df93bc1c68c2f84716687b15aa8e98b
MStroievus <108293369+MStroievus@users.noreply.github.com>
2023-11-09 21:09:46 +0200

    Returning Values

--- Sections_2/assets/scripts/app.js
+++ Sections_2/assets/scripts/app.js
@@ -1,6 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function addNumber(num1, num2) {
+  const result = num1 + num2;
+  alert("this is  result dude ->" + result);
+}
+
+addNumber(1, 2);
+addNumber(212, 2);
+console.log(addNumber);
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) *3/2-1`;
