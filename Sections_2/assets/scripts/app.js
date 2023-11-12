284c30c0d8eb7395b23cecba23f7c13d6d9471ac
MStroievus <108293369+MStroievus@users.noreply.github.com>
2023-11-12 13:13:11 +0200

    Exercises  6 - 7

--- Sections_2/assets/scripts/app.js
+++ Sections_2/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserInput() {
   return parseInt(userInput.value);
@@ -10,35 +11,43 @@ function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
   outputResult(currentResult, calcDescriptions);
 }
 
-function addNumber() {
+function add() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   createAndWriteLog("+", initialResult, enteredNumber);
+  const logEntry = {
+    operation: "ADD",
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
 function subtract() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -= enteredNumber;
   createAndWriteLog("-", initialResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *= enteredNumber;
   createAndWriteLog("*", initialResult, enteredNumber);
 }
 
 function divide() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /= enteredNumber;
   createAndWriteLog("/", initialResult, enteredNumber);
 }
 
-addBtn.addEventListener("click", addNumber);
+addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
 multiplyBtn.addEventListener("click", multiply);
 divideBtn.addEventListener("click", divide);
---
+++ Sections_2/exercise_6/exercise_6.js
@@ -0,0 +1,10 @@
+//? Exercise: Arrays
+// For this exercise, your task is to push two new numbers onto the existing numbers array: 8 and 2, in that order.
+
+// Don't create a new array, don't overwrite the existing array - instead manipulate the existing array as taught earlier in the course.
+
+let numbers = [10, 3, -1];
+
+function solve() {
+  numbers.push(...[8, 2]);
+}
