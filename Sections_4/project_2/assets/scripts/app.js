9977543b2541ba87bf6cd5da32460d30f52bfdc1
MStroievus <108293369+MStroievus@users.noreply.github.com>
2023-12-11 20:42:23 +0200

    Adding More "if" Statements & A "Strong Attack" Functionality

--- Sections_4/project_2/assets/scripts/app.js
+++ Sections_4/project_2/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 20;
-const MONSTER_ATTACK_VALUE = 21;
+const STRONG_ATTACK_VALUE = 25;
+const MONSTER_ATTACK_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterLife = chosenMaxLife;
@@ -7,18 +8,33 @@ let currentPlayerLife = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandle() {
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage === STRONG_ATTACK_VALUE;
+  }
   const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterLife -= monsterDamage;
-
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerLife -= playerDamage;
-
-  if (currentMonsterLife <= 0) {
+  if (currentMonsterLife <= 0 && currentPlayerLife > 0) {
     return alert("monster lost this game");
-  } else if (currentPlayerLife <= 0) {
+  } else if (currentPlayerLife <= 0 && currentMonsterLife > 0) {
     return alert("You are loser");
+  } else if (currentMonsterLife <= 0 && currentPlayerLife <= 0) {
+    return alert("draw");
   }
 }
 
+function attackHandle() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandle() {
+  attackMonster("STRONG_ATTACK");
+}
+
 attackBtn.addEventListener("click", attackHandle);
+strongAttackBtn.addEventListener("click", strongAttackHandle);
